ee143338b6d6b650aad34c8a53692edc7b9dc7d8
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-06-06 00:36:47 +0630

    generic view

--- test_API/api/models.py
+++ test_API/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    published = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
         return self.title
